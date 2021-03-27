4594ec29e6f3f24ed1abc94f3245ff00d7e5aeb3
Nathan Bloomfield <nathan.bloomfield@a8c.com>
2021-03-27 14:01:20 -0500

    Add demo module (#33)
    Adds a module of utilities for interactive testing. This is handy during development and debugging.
    
    Co-authored-by: nbloomf <nathan.bloomfield@automattic.com>

---
+++ app/ReplDemo.hs
@@ -0,0 +1,57 @@
+module Main where
+
+import Web.Api.WebDriver
+
+{-
+Utilities for demoing webdriver sessions. I mostly use this for testing
+and development. To run an individual demo, first make sure your remote end
+(chromedriver or geckodriver) is running on the correct port, then from ghci
+say something like:
+
+    withChromedriver demoNewWindow
+
+The executable runs all the demos, although this is less useful.
+-}
+
+main :: IO ()
+main = do
+  withChromedriver demoNewWindow
+
+
+
+
+
+withChromedriver :: WebDriverT IO () -> IO ()
+withChromedriver acts = do
+  execWebDriverT chromeConfig $
+    runIsolated_ defaultChromeCapabilities acts
+  return ()
+
+chromeConfig :: WebDriverConfig IO
+chromeConfig = defaultWebDriverConfig
+  { _environment = defaultWebDriverEnvironment
+    { _env = defaultWDEnv
+      { _remotePort = 9515
+      , _responseFormat = SpecFormat
+      }
+    }
+  }
+
+
+
+demoNewWindow :: WebDriverT IO ()
+demoNewWindow = do
+  -- open google.com in the current tab
+  navigateTo "https://www.google.com"
+
+  -- open a new tab; but do not switch to it
+  (handle, _) <- newWindow TabContext
+
+  -- switch to the new tab
+  switchToWindow handle
+
+  -- open bing.com in this tab
+  navigateTo "https://www.bing.com"
+
+  wait 5000000
+  return ()
